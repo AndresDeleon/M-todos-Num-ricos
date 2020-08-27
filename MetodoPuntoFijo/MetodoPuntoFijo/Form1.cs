d2ac726d2511430ba4f80d146291de2d28357e3e
Andrés Enrique Lemus Deleón <53536791+AndresDeleon@users.noreply.github.com>
2020-08-27 18:14:24 -0500

    Código Solución Método del Punto Fijo
    Código de solución a un ejercicio con métodos numéricos mediante el método especificado

---
+++ MetodoPuntoFijo/MetodoPuntoFijo/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MetodoRegulaFalsi
+{
+    public partial class Form1 : Form
+    {
+
+        public int i = 0;
+        public double xI = 0;
+        public double xGxI = 0;
+        private List<iteraciones> listIte { get; set; }
+
+        public Form1()
+        {
+            listIte = DoIter();
+            InitializeComponent();
+            this.CenterToScreen();
+        }
+
+        public double calcXGxI(double xI)
+        {
+            return Math.Exp(-xI);
+        }
+
+        public double calcError(double xIN, double xIA)
+        {
+            return Math.Abs((xIN - xIA) / xIN) * 100;
+        }
+
+        private List<iteraciones> DoIter()
+        {
+            var list = new List<iteraciones>();
+            xGxI = calcXGxI(xI);
+            list.Add(new iteraciones(1, xI, xGxI));
+            double error, comp;
+            int it = 1;
+            do
+            {
+                iteraciones anterior = list.Last();
+                it++;
+                xI = anterior.xGxI;
+                xGxI = calcXGxI(xI);
+                error = calcError(anterior.xGxI, anterior.xI);
+                comp = Math.Truncate(error * 1000);
+                list.Add(new iteraciones(it, xI, xGxI, error));
+            }
+            while ((comp / 1000) != 0.001 || (comp / 1000) < 0.001);
+
+            return list;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var ite = listIte;
+            iteraciones iF = ite.Last();
+            this.textBox1.Text = iF.xGxI.ToString();
+            tabla.DataSource = ite;
+        }
+
+    }
+}
---
+++ MetodoPuntoFijo/MetodoPuntoFijo/iteraciones.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoRegulaFalsi
+{
+    class iteraciones
+    {
+
+        public int i { get; set; }
+        public double xI { get; set; }
+        public double xGxI { get; set; }
+        public double error { get; set; }
+
+        public iteraciones() { }
+
+        public iteraciones(int i, double xI, double xGxI)
+        {
+            this.i = i;
+            this.xI = xI;
+            this.xGxI = xGxI;
+
+        }
+
+        public iteraciones(int i, double xI, double xGxI, double error)
+        {
+            this.i = i;
+            this.xI = xI;
+            this.xGxI = xGxI;
+            this.error = error;
+        }
+
+    }
+}

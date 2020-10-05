77a709a850e5d63bef9164516a4e1660d7833e81
Andrés Enrique Lemus Deleón <53536791+AndresDeleon@users.noreply.github.com>
2020-10-05 17:23:22 -0500

    Código de Matriz Adjunta
    Código en el cual se realizó una matriz adjunta a partir de una matriz base.

---
+++ MatrizAdjunta/MatrizAdjunta/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace MatrizAdjunta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.matrizAdjunta = new System.Windows.Forms.DataGridView();
+            this.matrizInicial = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.matrizAdjunta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrizInicial)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matriz a resolver:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(474, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Matriz Adjunta:";
+            // 
+            // matrizAdjunta
+            // 
+            this.matrizAdjunta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrizAdjunta.Location = new System.Drawing.Point(479, 151);
+            this.matrizAdjunta.Name = "matrizAdjunta";
+            this.matrizAdjunta.RowHeadersWidth = 51;
+            this.matrizAdjunta.RowTemplate.Height = 24;
+            this.matrizAdjunta.Size = new System.Drawing.Size(240, 150);
+            this.matrizAdjunta.TabIndex = 3;
+            // 
+            // matrizInicial
+            // 
+            this.matrizInicial.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrizInicial.Location = new System.Drawing.Point(34, 151);
+            this.matrizInicial.Name = "matrizInicial";
+            this.matrizInicial.RowHeadersWidth = 51;
+            this.matrizInicial.RowTemplate.Height = 24;
+            this.matrizInicial.Size = new System.Drawing.Size(240, 150);
+            this.matrizInicial.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(395, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Método de la Matriz Adjunta";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.matrizInicial);
+            this.Controls.Add(this.matrizAdjunta);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.matrizAdjunta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrizInicial)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView matrizAdjunta;
+        private System.Windows.Forms.DataGridView matrizInicial;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MatrizAdjunta/MatrizAdjunta/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrizAdjunta
+{
+
+    public partial class Form1 : Form
+    {
+        public static int tam = 3;
+        public double[,] matrizI = new double[tam,tam];
+        public double[,] matrizC = new double[tam, tam];
+        public double[,] matrizA = new double[tam, tam];
+
+        public Form1()
+        {
+            InitializeComponent();
+            CenterToScreen();
+        }
+
+        public void matrizCofactor()
+        {
+            int f = 0, c = 0, fD, cD;
+            for (int k = 0; k < (tam*tam); k++)
+            {
+                fD = 0;
+                cD = 0;
+                double[,] det = new double[(tam - 1), (tam - 1)];
+                for (int i = 0; i < tam; i++)
+                {
+                    for (int j = 0; j < tam; j++)
+                    {
+                        if (i != f && j != c)
+                        {
+                            det[fD, cD] = matrizI[i, j];
+                            cD++;
+
+                            if (cD == (tam-1))
+                            {
+                                cD = 0;
+                                if ((fD + 1) != (tam - 1))
+                                {
+                                    fD++;
+                                }
+                            }
+                        }
+                    }
+                }
+                if (((f + 1)+(c + 1)) % 2 == 0)
+                {
+                    matrizC[f, c] = determinante(det);
+
+                }
+                else
+                {
+                    matrizC[f, c] = (-1) * determinante(det);
+                }
+
+                c++;
+                if (c == tam)
+                {
+                    c = 0;
+                    f++;
+                }
+            }
+            
+        }
+
+        public double determinante(double[,] det)
+        {
+            double determinante, diagonal1 = 1, diagonal2 = 1;
+            int f=0, c=0;
+            for (int i = 0; i < (tam-1); i++)
+            {
+                for (int j = 0; j < (tam-1); j++)
+                {
+                    if (i == f && j == c)
+                    {
+                        diagonal1 = diagonal1 * det[i, j];
+                        f++;
+                        c++;
+                    }
+                    
+                }
+            }
+            f = 0;
+            c = (tam - 1) - 1;
+            for (int i = 0; i < (tam - 1); i++)
+            {
+                for (int j = 0; j < (tam - 1); j++)
+                {
+                    if (i == f && j == c)
+                    {
+                        diagonal2 = diagonal2 * det[i, j];
+                        f++;
+                        c--;
+                    }
+                }
+            }
+
+            determinante = diagonal1 - diagonal2;
+            return determinante;
+        }
+
+        public void cargarMatrizAdjunta()
+        {
+            for (int i = 0; i < tam; i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    matrizA[j, i] = matrizC[i, j];
+                }
+            }
+        }
+
+        public void cargarMatriz()
+        {
+            matrizInicial.Columns.Clear();
+            matrizAdjunta.Columns.Clear();
+            matrizI[0, 0] = 1;
+            matrizI[0, 1] = 0;
+            matrizI[0, 2] = -2;
+            matrizI[1, 0] = 3;
+            matrizI[1, 1] = 1;
+            matrizI[1, 2] = 2;
+            matrizI[2, 0] = 0;
+            matrizI[2, 1] = -1;
+            matrizI[2, 2] = -4;
+            matrizCofactor();
+            cargarMatrizAdjunta();
+            int n = 1;
+            while (n <= tam)
+            {
+                DataGridViewColumn columna = new DataGridViewColumn(new DataGridViewTextBoxCell());
+                columna.Width = 42;
+                this.matrizInicial.Columns.Add(columna);
+                DataGridViewColumn columna2 = new DataGridViewColumn(new DataGridViewTextBoxCell());
+                columna2.Width = 42;
+                this.matrizAdjunta.Columns.Add(columna2);
+                n++;
+            }
+
+            matrizInicial.Rows.Add(tam);
+            matrizAdjunta.Rows.Add(tam);
+            for (int i = 0; i < tam; i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    matrizInicial[i, j].Value = matrizI[i, j];
+                    matrizAdjunta[j, i].Value = matrizA[i, j];
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargarMatriz();
+        }
+    }
+}

7541d8ee457b8836d853bc86791667f16d120d27
Andrés Enrique Lemus Deleón <53536791+AndresDeleon@users.noreply.github.com>
2020-10-05 21:17:40 -0500

    Código Regla de Cramer
    Código con la solución de un sistema de ecuaciones utilizando la regla de Cramer

---
+++ ReglaCramer/ReglaCramer/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace MatrizAdjunta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.matrizInicial = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.txtZ = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.matrizInicial)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matriz a resolver:";
+            // 
+            // matrizInicial
+            // 
+            this.matrizInicial.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrizInicial.Location = new System.Drawing.Point(137, 166);
+            this.matrizInicial.Name = "matrizInicial";
+            this.matrizInicial.RowHeadersWidth = 51;
+            this.matrizInicial.RowTemplate.Height = 24;
+            this.matrizInicial.Size = new System.Drawing.Size(240, 150);
+            this.matrizInicial.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(29, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(589, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Resolución de matriz por Regla de Cramer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(500, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "X =";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(500, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Y =";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(499, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Z =";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(549, 170);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 22);
+            this.txtX.TabIndex = 9;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(548, 232);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 22);
+            this.txtY.TabIndex = 10;
+            // 
+            // txtZ
+            // 
+            this.txtZ.Location = new System.Drawing.Point(549, 294);
+            this.txtZ.Name = "txtZ";
+            this.txtZ.Size = new System.Drawing.Size(100, 22);
+            this.txtZ.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtZ);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.matrizInicial);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.matrizInicial)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView matrizInicial;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.TextBox txtZ;
+    }
+}
+
---
+++ ReglaCramer/ReglaCramer/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrizAdjunta
+{
+
+    public partial class Form1 : Form
+    {
+        public static int tam = 3;
+        public double[,] matriz = new double[tam,tam];
+        public double[,] matrizTemp = new double[tam, tam];
+        public double[,] matrizDet = new double[(tam + 2), tam];
+        public double detM, detX, detY, detZ, x, y, z;
+        public double[] constante = new double[] { 2, -3, 5 };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            CenterToScreen();
+        }
+
+        public double calcInc(double inc)
+        {
+            return inc / detM;
+        }
+
+        public double calMatrizInc(int n)
+        {
+            int c = 0;
+            for (int i = 0; i < tam; i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    if (j == n)
+                    {
+                        matrizTemp[i, j] = constante[c];
+                        c++;
+                    }
+                    else
+                    {
+                        matrizTemp[i, j] = matriz[i, j];
+                    }
+                }
+            }
+            return determinante(calcDet(matrizTemp));
+        }
+
+        public double[,] calcDet(double[,] m)
+        {
+            double[,] md = new double[tam + 2, tam];
+            for (int i = 0; i < tam; i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    md[i, j] = m[i, j];
+                }
+            }
+            int n = 0;
+            for (int i = 3; i < (tam + 2); i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    md[i, j] = m[n, j];
+                }
+                n++;
+            }
+            return md;
+        }
+
+        public void proceso()
+        {
+            matrizDet = calcDet(matriz);
+            detM = determinante(matrizDet);
+            detX = calMatrizInc(0);
+            detY = calMatrizInc(1);
+            detZ = calMatrizInc(2);
+            x = calcInc(detX);
+            y = calcInc(detY);
+            z = calcInc(detZ);
+        }
+
+        public double determinante(double[,] det)
+        {
+            double determinante, diagonal1, diagonal2, p1 = 0, p2 = 0;
+            int f, c,s=0;
+            for (int k = 0; k < 3; k++)
+            {
+                f = 0 + s;
+                c = 0;
+                diagonal1 = 1;
+                for (int i = 0; i < (tam + 2); i++)
+                {
+                    for (int j = 0; j < tam; j++)
+                    {
+                        if (i == f && j == c)
+                        {
+                            diagonal1 *= det[f, j];
+                            f++;
+                            c++;
+                        }
+                    }
+                }
+                p1 = p1 + diagonal1;
+                s++;
+            }
+            
+            s = 0;
+            for (int k = 0; k < 3; k++)
+            {
+                f = 0 + s;
+                c = (tam - 1);
+                diagonal2 = 1;
+                for (int i = 0; i < (tam + 2); i++)
+                {
+                    for (int j = 0; j < tam ; j++)
+                    {
+                        if (i == f && j == c)
+                        {
+                            diagonal2 *=  det[f, j];
+                            f++;
+                            c--;
+                        }
+                    }
+                }
+                p2 = p2 + diagonal2;
+                s++;
+            }
+            
+
+            determinante = p1 - p2;
+            return determinante;
+        }
+
+        public void cargarMatriz()
+        {
+            matrizInicial.Columns.Clear();
+            matriz[0, 0] = 2;
+            matriz[0, 1] = -1;
+            matriz[0, 2] = 5;
+            matriz[1, 0] = 3;
+            matriz[1, 1] = -2;
+            matriz[1, 2] = 1;
+            matriz[2, 0] = -2;
+            matriz[2, 1] = 2;
+            matriz[2, 2] = 3;
+            proceso();
+            int n = 1;
+            while (n <= tam)
+            {
+                DataGridViewColumn columna = new DataGridViewColumn(new DataGridViewTextBoxCell());
+                columna.Width = 42;
+                this.matrizInicial.Columns.Add(columna);
+                n++;
+            }
+
+            matrizInicial.Rows.Add(tam);
+            for (int i = 0; i < tam; i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    matrizInicial[i, j].Value = matriz[i, j];
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargarMatriz();
+            txtX.Text = Convert.ToString(x);
+            txtY.Text = Convert.ToString(y);
+            txtZ.Text = Convert.ToString(z);
+        }
+    }
+}

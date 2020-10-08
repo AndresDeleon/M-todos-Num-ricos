e42c770b44ebe99e6eb45945fcab14e277d96c09
Andrés Enrique Lemus Deleón <53536791+AndresDeleon@users.noreply.github.com>
2020-10-07 22:24:34 -0500

    Código de Descomposición LU
    Código solución a un sistema de ecuaciones

---
+++ DescomposicionLU/DescomposicionLU/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace MatrizAdjunta
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.matrizU = new System.Windows.Forms.DataGridView();
+            this.matrizInicial = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.matrizL = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtZ = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.matrizU)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrizInicial)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrizL)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Matriz a resolver:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(278, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Matriz U:";
+            // 
+            // matrizU
+            // 
+            this.matrizU.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrizU.Location = new System.Drawing.Point(283, 151);
+            this.matrizU.Name = "matrizU";
+            this.matrizU.RowHeadersWidth = 51;
+            this.matrizU.RowTemplate.Height = 24;
+            this.matrizU.Size = new System.Drawing.Size(240, 150);
+            this.matrizU.TabIndex = 3;
+            // 
+            // matrizInicial
+            // 
+            this.matrizInicial.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrizInicial.Location = new System.Drawing.Point(17, 151);
+            this.matrizInicial.Name = "matrizInicial";
+            this.matrizInicial.RowHeadersWidth = 51;
+            this.matrizInicial.RowTemplate.Height = 24;
+            this.matrizInicial.Size = new System.Drawing.Size(240, 150);
+            this.matrizInicial.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(11, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(284, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Descomposición LU";
+            // 
+            // matrizL
+            // 
+            this.matrizL.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.matrizL.Location = new System.Drawing.Point(548, 151);
+            this.matrizL.Name = "matrizL";
+            this.matrizL.RowHeadersWidth = 51;
+            this.matrizL.RowTemplate.Height = 24;
+            this.matrizL.Size = new System.Drawing.Size(240, 150);
+            this.matrizL.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(543, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Matriz L:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(23, 378);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(43, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "X =";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(73, 380);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 22);
+            this.txtX.TabIndex = 9;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(242, 380);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 22);
+            this.txtY.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(192, 378);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 25);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Y =";
+            // 
+            // txtZ
+            // 
+            this.txtZ.Location = new System.Drawing.Point(417, 382);
+            this.txtZ.Name = "txtZ";
+            this.txtZ.Size = new System.Drawing.Size(100, 22);
+            this.txtZ.TabIndex = 13;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(367, 380);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 25);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Z =";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(23, 328);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 25);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Solución:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtZ);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.matrizL);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.matrizInicial);
+            this.Controls.Add(this.matrizU);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.matrizU)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrizInicial)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.matrizL)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView matrizU;
+        private System.Windows.Forms.DataGridView matrizInicial;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView matrizL;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtZ;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ DescomposicionLU/DescomposicionLU/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatrizAdjunta
+{
+
+    public partial class Form1 : Form
+    {
+        public static int tam = 3;
+        public int col = 0, fil = 0;
+        public double[] f = new double[tam];
+        public double[] incognita = new double[tam];
+        public double[] det1 = new double[] { 7.85, -19.3, 71.4 };
+        public double[] det2 = new double[tam];
+        public double[,] matrizU1 = new double[tam,tam];
+        public double[,] matrizTemp = new double[tam, tam];
+        public double[,] matrizU2 = new double[tam, tam];
+        public double[,] matrizL1 = new double[tam, tam];
+        public double[,] matrizA = new double[tam, tam];
+
+        public Form1()
+        {
+            InitializeComponent();
+            CenterToScreen();
+        }
+
+        public void calcMatrizU()
+        {
+            matrizTemp = matrizA;
+            int n=0;
+            for (int k = 0; k < (tam - 1); k++)
+            {
+                double c1 = matrizTemp[col, fil], c2 = 0;
+                for (int i = 0; i < tam; i++)
+                {
+                    for (int j = 0; j < tam; j++)
+                    {
+                        if (i <= col)
+                        {
+                            matrizU1[i, j] = matrizTemp[i, j];
+                        }
+                        else
+                        {
+                            if (j == col)
+                            {
+                                c2 = matrizTemp[i, j];
+                                f[n] = c2 / c1;
+                                n++;
+                            }
+                            matrizU1[i, j] = matrizTemp[i, j] - ((c2 / c1) * matrizTemp[(fil), j]);
+                        }
+                    }
+                }
+                col++;
+                fil++;
+                matrizTemp = matrizU1;
+            }
+
+        }
+
+        public void calcMatrizL()
+        {
+            int c, f = 0, d = 0, a = 0;
+            for (int i = 0; i < tam; i++)
+            {
+                if (i != 0)
+                {
+                    f = i;
+                }
+                c = 0;
+                for (int j = 0; j < tam; j++)
+                {
+                    if (i == d && j == d)
+                    {
+                        matrizL1[i, j] = 1;
+                        d++;
+                    }
+                    else
+                    {
+                        if (c < f)
+                        {
+                            matrizL1[i, j] = this.f[a];
+                            c++;
+                            a++;
+                        }
+                        else
+                        {
+                            matrizL1[i, j] = 0;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void calcDet()
+        {
+            int f = 0;
+            for (int i = 0; i < tam; i++)
+            {
+                double resto = 0;
+                for (int j = 0; j < tam; j++)
+                {
+                    if (matrizL1[i, j] != 0 && i == f)
+                    {
+                        if (i != 0)
+                        {
+                            resto += det2[j] * matrizL1[i, j];
+                        }
+                    }
+                }
+                det2[i] = det1[i] - resto;
+                f++;
+            }
+        }
+
+        public void calcInc()
+        {
+            calcDet();
+            int c = 0, f = (tam - 1);
+            for (int i = (tam - 1); i >= 0; i--)
+            {
+                double resto = 0;
+                for (int j = (tam - 1); j >= 0; j--)
+                {
+                    if (matrizU1[i, j] != 0 && i == f)
+                    {
+                        if (i != (tam - 1))
+                        {
+                            resto += matrizU1[i, j] * incognita[j];
+                        }
+                    }
+                }
+                incognita[i] = (det2[i] - resto) / matrizU1[i, f];
+                f--;
+            }
+        }
+
+
+        public void cargarMatriz()
+        {
+            matrizInicial.Columns.Clear();
+            matrizU.Columns.Clear();
+            matrizA[0, 0] = 3;
+            matrizA[0, 1] = -0.1;
+            matrizA[0, 2] = -0.2;
+            matrizA[1, 0] = 0.1;
+            matrizA[1, 1] = 7;
+            matrizA[1, 2] = -0.3;
+            matrizA[2, 0] = 0.3;
+            matrizA[2, 1] = -0.2;
+            matrizA[2, 2] = 10;
+            calcMatrizU();
+            calcMatrizL();
+            calcInc();
+            int n = 1;
+            while (n <= tam)
+            {
+                DataGridViewColumn columna = new DataGridViewColumn(new DataGridViewTextBoxCell());
+                columna.Width = 42;
+                this.matrizInicial.Columns.Add(columna);
+                DataGridViewColumn columna2 = new DataGridViewColumn(new DataGridViewTextBoxCell());
+                columna2.Width = 42;
+                this.matrizU.Columns.Add(columna2);
+                DataGridViewColumn columna3 = new DataGridViewColumn(new DataGridViewTextBoxCell());
+                columna3.Width = 42;
+                this.matrizL.Columns.Add(columna3);
+                n++;
+            }
+
+            matrizInicial.Rows.Add(tam);
+            matrizU.Rows.Add(tam);
+            matrizL.Rows.Add(tam);
+            for (int i = 0; i < tam; i++)
+            {
+                for (int j = 0; j < tam; j++)
+                {
+                    matrizInicial[i, j].Value = matrizA[i, j];
+                    matrizU[j, i].Value = matrizU1[i, j];
+                    matrizL[i, j].Value = matrizL1[j, i];
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cargarMatriz();
+            txtX.Text = Convert.ToString(incognita[0]);
+            txtY.Text = Convert.ToString(incognita[1]);
+            txtZ.Text = Convert.ToString(incognita[2]);
+        }
+    }
+}
